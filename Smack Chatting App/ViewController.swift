61d5939a33c28f16a3d55e0ab6efa23aba728381
Ajinkya Sonar <Ajinkya@ajinkyas-macbook-pro.local>
2018-10-03 22:38:22 +0530

    Initial Commit

---
+++ Smack Chatting App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack Chatting App
+//
+//  Created by Ajinkya Sonar on 03/10/18.
+//  Copyright © 2018 Ajinkya Sonar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
